9d0f13d5bd5e52bdd3ff5faf5ebb23808a3d4069
Z-SPing <zspshsh@gmail.com>
2025-01-31 11:46:30 +0800

    Update proxy.js

--- functions/proxy.js
+++ functions/proxy.js
@@ -4,14 +4,23 @@ exports.handler = async (event) => {
   const targetPath = `https://api.groq.com/openai${path}`;
 
   try {
-    const response = await fetch(targetPath, {
+    // 根据HTTP方法判断是否需要请求体
+    const hasBody = ['POST', 'PUT', 'PATCH', 'DELETE'].includes(httpMethod);
+    const options = {
       method: httpMethod,
       headers: {
         ...headers,
         Host: 'api.groq.com',
+        // 确保在GET和HEAD方法中不发送Content-Type和请求体
+        ...(hasBody ? { 'Content-Type': headers['content-type'] } : {}),
       },
-      body: body,
-    });
+    };
+
+    if (hasBody && body) {
+      options.body = body;
+    }
+
+    const response = await fetch(targetPath, options);
 
     const responseBody = await response.text();
 
@@ -21,7 +30,7 @@ exports.handler = async (event) => {
         'Content-Type': response.headers.get('content-type'),
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST, PUT, DELETE'
+        'Access-Control-Allow-Methods': 'OPTIONS, GET, POST, PUT, DELETE',
       },
       body: responseBody,
     };
